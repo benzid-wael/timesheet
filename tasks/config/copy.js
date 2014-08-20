11165de6cd82cdcc811d2aed35451d713f66ca86
Wael BEN ZID <benzid.wael@hotmail.fr>
2014-08-20 11:24:54 +0100

    Support of AngularJS added

--- tasks/config/copy.js
+++ tasks/config/copy.js
@@ -13,26 +13,33 @@
  * For usage docs see:
  * 		https://github.com/gruntjs/grunt-contrib-copy
  */
+
 module.exports = function(grunt) {
-
 	grunt.config.set('copy', {
-		dev: {
-			files: [{
-				expand: true,
-				cwd: './assets',
-				src: ['**/*.!(coffee|less)'],
-				dest: '.tmp/public'
-			}]
-		},
-		build: {
-			files: [{
-				expand: true,
-				cwd: '.tmp/public',
-				src: ['**/*'],
-				dest: 'www'
-			}]
-		}
+			dev: {
+					files: [{
+							expand: true,
+							cwd: './assets',
+							src: ['**/*.!(coffee|less)'],
+							dest: '.tmp/public'
+					},{
+							expand: true,
+							cwd: './bower_components',
+							src: [
+									'angular/angular.js', 'angular-route/angular-route.js','angular-mocks/angular-mocks.js', 'angular-loader/angular-loader.js',
+							],
+							flatten: true,
+							dest: '.tmp/public/js/dependencies'
+					}]
+			},
+			build: {
+					files: [{
+							expand: true,
+							cwd: '.tmp/public',
+							src: ['**/*'],
+							dest: 'www'
+					}]
+			}
 	});
-
 	grunt.loadNpmTasks('grunt-contrib-copy');
 };
